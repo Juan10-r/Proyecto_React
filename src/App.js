616310a662d1b6b0edd627a2dffc4b1d5038dc7d
Juan10-r <juandarinconclavijo1007@gmail.com>
2025-07-08 11:02:10 -0500

    mi primer proyecto react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPages/LoginPage';
+import Recuperar from './pages/Recuperar/Recuperar';
+import Registrar from './pages/Registrar/Registrar';
+import PaginaPrincipal from './pages/PaginaPrincipal/PaginaPrincipal';
+
+
 
 function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage/>}/>
+      <Route path="/Recuperar" element={<Recuperar/>}/>
+      <Route path="/Registrar" element={<Registrar/>}/>
+      <Route path="/PaginaPrincipal" element={<PaginaPrincipal/>}/>
+      </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App;
